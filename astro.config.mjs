4a780b75f1692e82dbf07916edf0a78da72ab022
Vendaarlia <94086273+Vendaarlia@users.noreply.github.com>
2026-04-21 21:02:47 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import cloudflare from '@astrojs/cloudflare';
 import dotenv from 'dotenv';
-
-import vercel from '@astrojs/vercel';
 
 dotenv.config();
 
 export default defineConfig({
   integrations: [vue()],
   output: 'server',
-
+  adapter: cloudflare(),
   server: {
     port: 3000
-  },
-
-  adapter: vercel()
+  }
 });
